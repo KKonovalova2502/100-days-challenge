c8f47026fd680013e2b7d769e6bbf72442e3e9b1
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-07-01 00:52:22 +0300

    day 4 done

--- 004-hidden search widget/script.js
+++ 004-hidden search widget/script.js
@@ -1,8 +1,11 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
+const icon = document.querySelector(".fas");
 
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
   input.focus();
+  icon.classList.toggle("fa-search-plus", !search.classList.contains("active"));
+  icon.classList.toggle("fa-times", search.classList.contains("active"));
 });
